c866d3f877f95aa3d9b3cf854b184987ab0c2c49
VikasVerma-15 <97469119+VikasVerma-15@users.noreply.github.com>
2025-12-29 17:14:13 +0530

    Add state management for todos in App component

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,30 @@
-
-import React from "react";
+import React, { useState } from "react";
+import TodoList from "./TodoList";
 import './../styles/App.css';
 
 const App = () => {
+  // Parent state
+  const [todos, setTodos] = useState([
+    { id: 1, text: "Learn React", completed: false },
+    { id: 2, text: "Build a Todo App", completed: false },
+    { id: 3, text: "Master Lifting State Up", completed: false },
+  ]);
+
+  // Handler to mark a todo as complete
+  const handleComplete = (id) => {
+    setTodos(
+      todos.map(todo =>
+        todo.id === id ? { ...todo, completed: true } : todo
+      )
+    );
+  };
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      <h1>Todo List</h1>
+      <TodoList todos={todos} handleComplete={handleComplete} />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;

984c4f646e81db104f7d1cd70065db084b6e8688
VikasVerma-15 <97469119+VikasVerma-15@users.noreply.github.com>
2025-12-29 17:16:30 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,17 +3,15 @@ import TodoList from "./TodoList";
 import './../styles/App.css';
 
 const App = () => {
-  // Parent state
   const [todos, setTodos] = useState([
     { id: 1, text: "Learn React", completed: false },
     { id: 2, text: "Build a Todo App", completed: false },
     { id: 3, text: "Master Lifting State Up", completed: false },
   ]);
 
-  // Handler to mark a todo as complete
   const handleComplete = (id) => {
-    setTodos(
-      todos.map(todo =>
+    setTodos((prevTodos) =>
+      prevTodos.map((todo) =>
         todo.id === id ? { ...todo, completed: true } : todo
       )
     );
@@ -21,10 +19,10 @@ const App = () => {
 
   return (
     <div>
-      <h1>Todo List</h1>
       <TodoList todos={todos} handleComplete={handleComplete} />
     </div>
   );
 };
 
 export default App;
+

ce0aa9097f4883f01f30c7a81ea4761cfb51bb43
Vikas Verma <vikasverma15@gmail.com>
2026-01-12 12:09:50 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,2 +1,22 @@
-import App from "./components/App";
+import React, { useState } from "react";
+import TodoList from "./components/TodoList";
+
+const App = () => {
+  const [todos, setTodos] = useState([
+    { id: 1, text: "Learn React", completed: false }
+  ]);
+
+  const handleComplete = (id) => {
+    setTodos(
+      todos.map(todo =>
+        todo.id === id
+          ? { ...todo, completed: true }
+          : todo
+      )
+    );
+  };
+
+  return <TodoList todos={todos} handleComplete={handleComplete} />;
+};
+
 export default App;
